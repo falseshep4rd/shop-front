0545163755f42b742efc9272f1ac1370714e2091
Saras <sarazzz123@gmail.com>
2023-01-26 01:24:13 +0200

    shop frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './layout/Navbar'
+import Home from './pages/Home'
+import AddProduct from './products/AddProduct';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import EditProduct from './products/EditProduct';
+import ViewProduct from './products/ViewProduct';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/addproduct" element={<AddProduct />}></Route>
+          <Route exact path="/editproduct/:id" element={<EditProduct/>}></Route>
+          <Route exact path="/viewproduct/:id" element={<ViewProduct/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/products/EditProduct.js
@@ -0,0 +1,79 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+export default function EditProducts() {
+    let navigate = useNavigate()
+    const { id } = useParams()
+
+    const [product, setProduct] = useState({
+        name: "",
+        price: "",
+    });
+
+    const { name, price } = product;
+
+    const onInputChange = (e) => {
+        setProduct({ ...product, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        await axios.put(`http://localhost:8080/product/${id}`, product);
+        navigate("/");
+    };
+
+    useEffect(() => {
+        loadProduct()
+    }, [])
+
+    const loadProduct = async () => {
+        const result = await axios.get(`http://localhost:8080/product/${id}`)
+        setProduct(result.data)
+    }
+
+    return (
+        <div className='container'>
+            <div className='row'>
+                <div className='col-md-6 offset-md-3 border rounded p-4 mt-2'>
+                    <h2 className='text-center m-4'>Edit product</h2>
+
+                    <form onSubmit={(e) => onSubmit(e)}>
+                        <div className="mb-3">
+                            <label htmlFor="Name" className="form-label">
+                                Name
+                            </label>
+                            <input
+                                type={"text"}
+                                className="form-control"
+                                placeholder="Enter product's name"
+                                name="name"
+                                value={name}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="Price" className="form-label">
+                                Price(EUR)
+                            </label>
+                            <input
+                                type={"double"}
+                                className="form-control"
+                                placeholder="Enter product's price"
+                                name="price"
+                                value={price}
+                                onChange={(e) => onInputChange(e)}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-outline-primary">
+                            Submit
+                        </button>
+                        <Link className="btn btn-outline-danger mx-2" to="/">
+                            Cancel
+                        </Link>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
